26061d05c495c54ed41b61a7e3fb043ebc7960d9
Akshaisali <akshaisali007@gmail.com>
2023-06-10 14:54:08 +0530

    green ->add templates

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
-app=Flask(__name__)
+from flask import render_template
 
-@app.route('/')
-def hello_world():
-    greeting="world"
-    return f'hello{greeting}!'
+app= Flask(__name__)
+
+@app.route("/")
+
+def index():
+    greeting="hello world"
+    return render_template("index.html", greeting= greeting)
+
 if __name__ == "__main__":
     app.run()
